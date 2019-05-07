25ada76cdbd58c42d00690c24007478826d654e9
youluna <njuzhaoguoyan@foxmail.com>
2019-05-07 16:00:21 +0800

    fix: window.Next is {}

--- content/webpack.config.js
+++ content/webpack.config.js
@@ -1,33 +1,40 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
-const CssSplitWebpackPlugin = require('css-split-webpack-plugin').default;
-const chalk = require('chalk');
-const babelConfig = require('@alifd/babel-preset-next')({}, { runtime: true });
-const autoprefixer = require('autoprefixer');
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
+const CssSplitWebpackPlugin = require("css-split-webpack-plugin").default;
+const chalk = require("chalk");
+const babelConfig = require("@alifd/babel-preset-next")({}, { runtime: true });
+const autoprefixer = require("autoprefixer");
 
-const css = (options = {}) => [{
-  loader: 'style-loader'
-}, {
-  loader: 'css-loader',
-  options: {
-    minimize: !!options.minimize
+const css = (options = {}) => [
+  {
+    loader: "style-loader"
+  },
+  {
+    loader: "css-loader",
+    options: {
+      minimize: !!options.minimize
+    }
+  },
+  {
+    loader: "postcss-loader",
+    options: {
+      plugins: () => [
+        autoprefixer({
+          browsers: ["last 2 versions", "Firefox ESR", "> 1%", "ie >= 9"]
+        })
+      ]
+    }
   }
-}, {
-  loader: 'postcss-loader',
-  options: {
-    plugins: () => [autoprefixer({
-      browsers: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 9']
-    })]
-  }
-}];
+];
 
 const scss = (options = {}) => [
-  ...css(options), {
-    loader: 'fast-sass-loader'
+  ...css(options),
+  {
+    loader: "fast-sass-loader"
   }
 ];
 
@@ -36,97 +43,108 @@ const buildTime = {
   end: new Date().getTime()
 };
 
-module.exports = function ({minimize = false}) {
-  const cwd = process.cwd();
-
+module.exports = function({ minimize = false }) {
   const config = {
     entry: {
-      next: ['./index.js', './index.scss'],
-      ['next-noreset']: './index-noreset.scss'
+      next: ["./index.scss", "./index.js"],
+      ["next-noreset"]: "./index-noreset.scss"
     },
     output: {
-      path: path.join(cwd, 'dist'),
-      publicPath: '/dist/',
-      library: 'Next',
-      libraryTarget: 'umd'
+      path: path.join(__dirname, "dist"),
+      publicPath: "/dist/",
+      library: "Next",
+      libraryTarget: "umd"
     },
     resolve: {
-      extensions: ['.js', '.jsx']
+      extensions: [".js", ".jsx"]
     },
     externals: [
       {
-        'react': {
-          root: 'React',
-          commonjs2: 'react',
-          commonjs: 'react',
-          amd: 'react'
+        react: {
+          root: "React",
+          commonjs2: "react",
+          commonjs: "react",
+          amd: "react"
         }
-      }, {
-        'react-dom': {
-          root: 'ReactDOM',
-          commonjs2: 'react-dom',
-          commonjs: 'react-dom',
-          amd: 'react-dom'
+      },
+      {
+        "react-dom": {
+          root: "ReactDOM",
+          commonjs2: "react-dom",
+          commonjs: "react-dom",
+          amd: "react-dom"
         }
-      }, {
-        'moment': {
-          root: 'moment',
-          commonjs2: 'moment',
-          commonjs: 'moment',
-          amd: 'moment'
+      },
+      {
+        moment: {
+          root: "moment",
+          commonjs2: "moment",
+          commonjs: "moment",
+          amd: "moment"
         }
       }
     ],
     module: {
-      rules: [{
-        test: /\.jsx?$/,
-        use: {
-          loader: 'babel-loader',
-          options: Object.assign({
-            cacheDirectory: true,
-          }, babelConfig)
+      rules: [
+        {
+          test: /\.jsx?$/,
+          use: {
+            loader: "babel-loader",
+            options: Object.assign(
+              {
+                cacheDirectory: true
+              },
+              babelConfig
+            )
+          },
+          exclude: /node_modules/
         },
-        exclude: /node_modules/,
-      }, {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: css({ minimize }).slice(1)
-        })
-      }, {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: scss({ minimize }).slice(1)
-        })
-      }]
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: css({ minimize }).slice(1)
+          })
+        },
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: scss({ minimize }).slice(1)
+          })
+        }
+      ]
     },
     plugins: [
+      new webpack.BannerPlugin(`{{name}}@{{version}} (https://fusion.design)
+{{library.npm}}@{{library.version}} (https://fusion.design)
+Copyright 2018-present Alibaba Group,
+Licensed under MIT (https://github.com/alibaba-fusion/next/blob/master/LICENSE)`),
       new webpack.NoEmitOnErrorsPlugin(),
       new webpack.optimize.ModuleConcatenationPlugin(),
       new CaseSensitivePathsPlugin(),
       new webpack.DefinePlugin({
-        'process.env.NODE_ENV': '"production"'
+        "process.env.NODE_ENV": '"production"'
       }),
       new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
       new webpack.ProgressPlugin((percentage, msg) => {
         if (percentage === 0) {
           buildTime.start = new Date().getTime();
-          console.log(chalk.blue('> webpack bundle is start.'));
+          console.log(chalk.blue("> webpack bundle is start."));
         }
         if (percentage === 1) {
           buildTime.end = new Date().getTime();
-          console.log(chalk.blue('> webpack bundle is finished. (Spent %s ms)'), buildTime.end - buildTime.start);
+          console.log(chalk.blue("> webpack bundle is finished. (Spent %s ms)"), buildTime.end - buildTime.start);
         }
       })
     ]
   };
 
-  if (minimize){
-    config.output.filename = '[name].min.js';
+  if (minimize) {
+    config.output.filename = "[name].min.js";
     config.plugins.push(
       new ExtractTextPlugin({
-        filename: '[name].min.css',
+        filename: "[name].min.css",
         allChunks: true
       }),
       new webpack.optimize.UglifyJsPlugin({
@@ -141,18 +159,16 @@ module.exports = function ({minimize = false}) {
       })
     );
   } else {
-    config.output.filename = '[name].js';
+    config.output.filename = "[name].js";
     config.plugins.push(
       new ExtractTextPlugin({
-        filename: '[name].css',
+        filename: "[name].css",
         allChunks: true
       })
     );
   }
 
-  config.plugins.push(
-    new CssSplitWebpackPlugin({size: 4000, preserve: true})
-  );
+  config.plugins.push(new CssSplitWebpackPlugin({ size: 4000, preserve: true }));
 
   return config;
 };
