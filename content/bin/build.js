b56f37784a5e0f2173f3983d4c2dc42be42a5917
皆虚 <jiarui.xjr@alibaba-inc.com>
2020-08-12 11:14:58 +0800

    feat: support css var

--- content/bin/build.js
+++ content/bin/build.js
@@ -1,14 +1,14 @@
-'use strict'
+"use strict";
 
-const path = require('path');
-const rmdir = require('rimraf');
-const webpack = require('webpack');
-const fs = require('fs');
+const path = require("path");
+const rmdir = require("rimraf");
+const webpack = require("webpack");
+const fs = require("fs");
 
 const BASE_DIR = path.dirname(__dirname);
 
-const config = require('../webpack.config.js');
-const distPath = path.join(process.cwd(), 'dist');
+const config = require("../webpack.config.js");
+const distPath = path.join(process.cwd(), "dist");
 
 function build(minimize = false) {
   return new Promise((resolve, reject) => {
@@ -18,9 +18,11 @@ function build(minimize = false) {
       } else {
         const errors = stats.toJson().errors;
         if (errors.length) {
-          reject(stats.toString({
-            chunkModules: false,
-          }));
+          reject(
+            stats.toString({
+              chunkModules: false,
+            })
+          );
         } else {
           resolve(stats);
         }
@@ -30,35 +32,38 @@ function build(minimize = false) {
 }
 
 function emendLess() {
-  let less = fs.readFileSync(path.join(BASE_DIR, 'variables.less'), 'utf8');
+  let less = fs.readFileSync(path.join(BASE_DIR, "variables.less"), "utf8");
 
-  less = less.replace(/\/\/ lessUnsupport[\s\S]*/g, '');
-  fs.writeFileSync(path.join(BASE_DIR, 'variables.less'), less)
+  less = less.replace(/\/\/ lessUnsupport[\s\S]*/g, "");
+  fs.writeFileSync(path.join(BASE_DIR, "variables.less"), less);
 }
 
 async function buildTheme() {
   // build
-  console.log('# build');
+  console.log("# build");
   await build();
-  console.log('# build minimize');
+  console.log("# build minimize");
   await build(true);
 
-  // 修正less 
-  console.log('# emend less')
-  emendLess()
+  // 修正less
+  console.log("# emend less");
+  emendLess();
 
   // remove bin dir
-  console.log('# remove bin dir');
-  rmdir.sync(path.join(BASE_DIR, 'webpack.config.js'));
-  rmdir.sync(path.join(BASE_DIR, 'bin'));
+  console.log("# remove bin dir");
+  rmdir.sync(path.join(BASE_DIR, "webpack.config.js"));
+  rmdir.sync(path.join(BASE_DIR, "bin"));
 
   try {
-    fs.unlinkSync(path.join(distPath, 'next-noreset.min.js'));
-    fs.unlinkSync(path.join(distPath, 'next-noreset.js'));
+    fs.unlinkSync(path.join(distPath, "next-noreset.min.js"));
+    fs.unlinkSync(path.join(distPath, "next-noreset.js"));
+    fs.unlinkSync(path.join(distPath, "next-noreset.var.min.js"));
+    fs.unlinkSync(path.join(distPath, "next-noreset.var.js"));
+    fs.unlinkSync(path.join(distPath, "next.var.min.js"));
+    fs.unlinkSync(path.join(distPath, "next.var.js"));
   } catch (e) {
-    console.log('remove next-noreset.js or next-noreset.min.js failed: ', e);
+    console.log("remove next-noreset.js or next-noreset.min.js failed: ", e);
   }
 }
 
 buildTheme();
-
--- content/webpack.config.js
+++ content/webpack.config.js
@@ -10,52 +10,52 @@ const autoprefixer = require("autoprefixer");
 
 const css = (options = {}) => [
   {
-    loader: "style-loader"
+    loader: "style-loader",
   },
   {
     loader: "css-loader",
     options: {
-      minimize: !!options.minimize
-    }
+      minimize: !!options.minimize,
+    },
   },
   {
     loader: "postcss-loader",
     options: {
       plugins: () => [
         autoprefixer({
-          browsers: ["last 2 versions", "Firefox ESR", "> 1%", "ie >= 9"]
-        })
-      ]
-    }
-  }
+          browsers: ["last 2 versions", "Firefox ESR", "> 1%", "ie >= 9"],
+        }),
+      ],
+    },
+  },
 ];
 
 const scss = (options = {}) => [
   ...css(options),
   {
-    loader: "fast-sass-loader"
-  }
+    loader: "fast-sass-loader",
+  },
 ];
 
 const buildTime = {
   start: new Date().getTime(),
-  end: new Date().getTime()
+  end: new Date().getTime(),
 };
 
 module.exports = function ({ minimize = false }) {
   const config = {
     entry: {
       next: ["./index.scss", "./index.js"],
-      ["next-noreset"]: "./index-noreset.scss"
+      ["next-noreset"]: "./index-noreset.scss",
     },
     output: {
       path: path.join(__dirname, "dist"),
       publicPath: "/dist/",
       library: "Next",
-      libraryTarget: "umd"
+      libraryTarget: "umd",
     },
     resolve: {
-      extensions: [".js", ".jsx"]
+      extensions: [".js", ".jsx"],
     },
     externals: [
       {
@@ -63,25 +63,25 @@ module.exports = function ({ minimize = false }) {
           root: "React",
           commonjs2: "react",
           commonjs: "react",
-          amd: "react"
-        }
+          amd: "react",
+        },
       },
       {
         "react-dom": {
           root: "ReactDOM",
           commonjs2: "react-dom",
           commonjs: "react-dom",
-          amd: "react-dom"
-        }
+          amd: "react-dom",
+        },
       },
       {
         moment: {
           root: "moment",
           commonjs2: "moment",
           commonjs: "moment",
-          amd: "moment"
-        }
-      }
+          amd: "moment",
+        },
+      },
     ],
     module: {
       rules: [
@@ -91,28 +91,28 @@ module.exports = function ({ minimize = false }) {
             loader: "babel-loader",
             options: Object.assign(
               {
-                cacheDirectory: true
+                cacheDirectory: true,
               },
               babelConfig
-            )
+            ),
           },
-          exclude: /node_modules/
+          exclude: /node_modules/,
         },
         {
           test: /\.css$/,
           use: ExtractTextPlugin.extract({
             fallback: "style-loader",
-            use: css({ minimize }).slice(1)
-          })
+            use: css({ minimize }).slice(1),
+          }),
         },
         {
           test: /\.scss$/,
           use: ExtractTextPlugin.extract({
             fallback: "style-loader",
-            use: scss({ minimize }).slice(1)
-          })
-        }
-      ]
+            use: scss({ minimize }).slice(1),
+          }),
+        },
+      ],
     },
     plugins: [
       new webpack.BannerPlugin(`{{name}}@{{version}} (https://fusion.design)
@@ -123,7 +123,7 @@ Licensed under MIT (https://github.com/alibaba-fusion/next/blob/master/LICENSE)`
       new webpack.optimize.ModuleConcatenationPlugin(),
       new CaseSensitivePathsPlugin(),
       new webpack.DefinePlugin({
-        "process.env.NODE_ENV": '"production"'
+        "process.env.NODE_ENV": '"production"',
       }),
       new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
       new webpack.ProgressPlugin((percentage, msg) => {
@@ -133,28 +133,38 @@ Licensed under MIT (https://github.com/alibaba-fusion/next/blob/master/LICENSE)`
         }
         if (percentage === 1) {
           buildTime.end = new Date().getTime();
-          console.log("> webpack bundle is finished. (Spent %s ms)", buildTime.end - buildTime.start);
+          console.log(
+            "> webpack bundle is finished. (Spent %s ms)",
+            buildTime.end - buildTime.start
+          );
         }
-      })
-    ]
+      }),
+    ],
   };
+
+  const { version } = require("@alifd/next/package.json");
+  //1.21.0版本新增next.var.css文件
+  if (version >= "1.21.0") {
+    config.entry["next-noreset.var"] = "./index-noreset.var.css";
+    config.entry["next.var"] = "./index.var.css";
+  }
 
   if (minimize) {
     config.output.filename = "[name].min.js";
     config.plugins.push(
       new ExtractTextPlugin({
         filename: "[name].min.css",
-        allChunks: true
+        allChunks: true,
       }),
       new webpack.optimize.UglifyJsPlugin({
         minimize: true,
         compress: {
-          warnings: false
+          warnings: false,
         },
         output: {
           comments: false,
-          ascii_only: true
-        }
+          ascii_only: true,
+        },
       })
     );
   } else {
@@ -162,12 +172,14 @@ Licensed under MIT (https://github.com/alibaba-fusion/next/blob/master/LICENSE)`
     config.plugins.push(
       new ExtractTextPlugin({
         filename: "[name].css",
-        allChunks: true
+        allChunks: true,
       })
     );
   }
 
-  config.plugins.push(new CssSplitWebpackPlugin({ size: 4000, preserve: true }));
+  config.plugins.push(
+    new CssSplitWebpackPlugin({ size: 4000, preserve: true })
+  );
 
   return config;
 };
